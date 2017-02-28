7e8f6a960755e1240a4580364ef30147f4ecb502
Danielle Walton <daniewalton@gmail.com>
2017-02-28 16:09:00 +0000

    Adds looping exercises

---
+++ loops.js
@@ -0,0 +1,10 @@
+for (var number = 100; number <= 200; number++ ) {
+  console.log(number);
+  if (number % 3 === 0); {
+    console.log("Loopy");
+  } else if {(number % 4 === 0);
+    console.log("Lighthouse");
+  } else {(number % 3 === 0 && % 4 === 0);
+    console.log("LoopyLighthouse");
+  }
+  }
